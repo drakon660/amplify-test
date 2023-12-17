373e9d243dd4ec041aa2366a8cd8d3aa40314cf4
drakon660 <drakon660@gmail.com>
2023-12-17 10:35:13 +0100

    init

--- samples/react/auth/authenticator/src/App.js
+++ samples/react/auth/authenticator/src/App.js
@@ -1,41 +1,28 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.svg';
-import Amplify from 'aws-amplify';
-import {
-	AmplifyAuthenticator,
-	AmplifyContainer,
-	AmplifySignOut,
-} from '@aws-amplify/ui-react';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { useAuthenticator, Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(awsconfig);
 
 const App = () => {
-	const [authState, setAuthState] = React.useState();
-	const [user, setUser] = React.useState();
 
-	React.useEffect(() => {
-		return onAuthUIStateChange((nextAuthState, authData) => {
-			setAuthState(nextAuthState);
-			setUser(authData);
-		});
-	}, []);
-
-	return authState === AuthState.SignedIn && user ? (
-		<div className="App">
-			<header className="App-header">
-				<div>Hello, {user.username}</div>
-				<img src={logo} className="App-logo" alt="logo" />
-				<AmplifySignOut />
-			</header>
-		</div>
-	) : (
-		<AmplifyContainer>
-			<AmplifyAuthenticator />
-		</AmplifyContainer>
-	);
+  // Use the value of authStatus to decide which page to render
+ return (
+    <>
+       <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+    </>
+  );
 };
 
 export default App;
